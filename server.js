c2a4edf1dc1f98167913520348179b7891b5aef9
fatihguzel <20060393@stu.omu.edu.tr>
2022-06-19 21:06:38 +0300

    UPDATE

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
+// Environment Variables
+dotenv.config({
+    path: "./config/env/config.env"
+})
 const app = express()
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT
 app.get("/",(req,res)=>{
     res.send("<h1>Hello Question Answer API<h1>")
 })
 app.listen(PORT,() => {
-    console.log(`APP Started on : ${PORT}`);
+    console.log(`APP Started on : ${PORT}: ${process.env.NODE_ENV}` );
 })
